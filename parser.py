15a19b1d6d43206e53cdfeb3a2f6a9b9ed494686
Spisocheg <spisocheg@users.noreply.github.com>
2026-02-19 04:03:19 +0300

    html solution done

--- parser.py
+++ parser.py
@@ -49,41 +49,53 @@ BODY4LOGIN = {
 }
 
 
-EVENTS_GETTER_URL = os.getenv('EVENTS_GETTER_URL')
-HEADERS4EVENTS = {
+MAIN_INFO_URL = os.getenv('MAIN_INFO_URL')
+HEADERS4MAIN_INFO = {
     "Sec-Ch-Ua": 'Not(A:Brand";v="8", "Chromium";v="144"',
     "Sec-Ch-Ua-Platform": "Windows",
     "Sec-Ch-Ua-Mobile": "?0",
     "Sec-Fetch-Site": "same-origin",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-User": "?1",
+    "Sec-Fetch-Dest": "frame",
     "Referer": "https://dot.mpei.ac.ru/close/students/info.asp",
-    "Priority": "u=1, i",
+    "Priority": "u=0, i",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36",
-    "X-Requested-With": "XMLHttpRequest",
     "Accept-Language": "ru-RU,ru;q=0.9",
-    "Accept": "application/xml, text/xml, */*; q=0.01",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "Accept-Encoding": "gzip, deflate, br",
+    "Upgrade-Insecure-Requests": "1",
 }
 
 
-def get_response_encoding(response: requests.Response) -> str:
-    return response.text.split(' ?>')[0].split('encoding=')[-1].strip('"')
+def extract_dates(s: str) -> tuple[str, str]:
+    if ':' in s:
+        start = datetime.now().strftime('%Y-%m-%d')
+        end = datetime.strptime(s.split('до')[-1].strip().split(' ')[0], '%d.%m.%Y').strftime('%Y-%m-%d')
+        return (start, end)
+    
+    s = s.lstrip('с ')
+    dates = tuple(map(lambda x: datetime.strptime(x.strip(), '%d.%m.%Y').strftime('%Y-%m-%d'), s.split('до')))
+    return tuple(dates)
 
 
-def extract_data(soup: BeautifulSoup) -> list[dict]:
-    xml_fields = {"eventType", "eventDateBegin", "eventDateEnd", "courseName", "elementName"}
-    event_type_being_collected = "event"        # if eventType==event : item will be collected
-    
-    data = []
-    for item in soup.find_all('item'):
-        if item.find("eventType").text == event_type_being_collected:
-            event = {}
-            for field in xml_fields:
-                event[field] = item.find(field).text
-            data.append(event)
-    
-    return data        
+def extract_events(soup: BeautifulSoup) -> list[dict]:
+    events_tables = soup.find('div', id='events').find_all('tbody')
+
+    events = []
+    for tbody in events_tables:
+        rows = tbody.find_all('tr')
+        for row in rows:
+            fields = row.find_all('td')
+            dates = extract_dates(fields[0].text)
+            events.append({
+                'date_start': dates[0],
+                'date_end': dates[-1],
+                'course_name': row.find('td', class_='course-name').text,
+                'event_name': row.find('td', class_='element-name').text,
+            })
+
+    return events    
 
 
 def get_auth_cookie() -> dict:
@@ -101,26 +113,25 @@ def get_auth_cookie() -> dict:
 
 def get_events_list(cookies: dict) -> list[dict]:
     """
-    sends a request to receive "Мои мероприятия", processes (encoding, formatting), returns data in the form of a dictionary list (json)
+    sends a request to receive main page, processes (extract events, formatting), returns data in the form of a dictionary list (json)
 
     Args:
         cookies (dict): auth cookies
 
     Returns:
         list[dict]: list of "my events"
-    """
+    """    
     logger.info('requesting list of events...')
-    response = requests.get(url=EVENTS_GETTER_URL,
-                            headers=HEADERS4EVENTS,
-                            cookies=cookies)
-    logger.info('"my events" list have been received')
+    response = requests.get(url=MAIN_INFO_URL,
+                            headers=HEADERS4MAIN_INFO, cookies=(cookies | {'etab': 'events'}))
+    logger.info('main page have been received')
+    soup = BeautifulSoup(response.content, "html.parser")
     
     logger.info('processing...')
-    encoding = get_response_encoding(response)                              # <?xml version="1.0" encoding="utf-8"?>
-    soup = BeautifulSoup(response.content, "xml", from_encoding=encoding)   # xml -> python_obj
-    data = extract_data(soup)                                               # clear: del extra fields and convert to dict
-    logger.info(f'items processed: {len(data)}')
-    return data
+    events = extract_events(soup)
+    logger.info(f'items processed: {len(events)}')
+    
+    return events
     
 
 def save_event_to_json(data: list[dict]):
